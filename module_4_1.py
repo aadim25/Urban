5ce7f8013439aa7f8fa12a4d32630756673fd122
aadim25 <164591588+aadim25@users.noreply.github.com>
2024-08-13 15:51:32 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as fake
+import true_math as true
+
+result1 = fake.divide (69, 3)
+result2 = fake.divide(3, 0)
+result3 = true.divide(49, 7)
+result4 = true.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
