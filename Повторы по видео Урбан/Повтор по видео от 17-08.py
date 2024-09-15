e2b01a042ad430a88dfe0ed45df655ef9c53aae4
aadim <aadim@mail.ru>
2024-09-15 22:07:41 +0700

    Urban Modul 5

--- Повторы по видео Урбан/Повтор по видео от 17-08.py
+++ Повторы по видео Урбан/Повтор по видео от 17-08.py
@@ -8,6 +8,7 @@ class Employee:
         self.position = position
 
     def get_info(self):
+        print(3)
         return f'Сотрудник: {self.name}, Должность: {self.position}'
 
 class Developer (Employee):
@@ -19,6 +20,7 @@ class Developer (Employee):
         self.programming_language = programming_language
 
     def get_info(self):
+        print(2)
         base_info = super().get_info()
         return f'{base_info}, Язык программирования {self.programming_language}'
 
@@ -33,21 +35,30 @@ class Manager(Employee):
 
 class RemoteWorker:
     def __init__(self, location):
+        print (4)
         self.location = location
 
     def get_location(self):
         return f'Работает удаленно из {self.location}'
 
 class RemoteDeveloper(Developer, RemoteWorker):
-    def __init__(self, name,position, programming_language,location):
-        super().__init__(name,position,programming_language,location)
+    def __init__(self, name, position, programming_language, location):
+        # super().__init__(name, position, programming_language, location)
+        # Developer.__init__(self, name, position, programming_language)
+        # RemoteWorker.__init__(self, location)
+        super(Employee, self).__init__(location)
+    def get_info(self):
+        print (1)
+        dev_info = super().get_info()
+        location_info = self.get_location()
+        return f'{dev_info}, {location_info}'
 
 # employee = Employee('Алиса', 'Менеджер')
-developer = Developer('Саша', 'Разработчик','Python')
-manager = Manager('Алиса','Менеджер', 20)
+# developer = Developer('Саша', 'Разработчик','Python')
+# manager = Manager('Алиса','Менеджер', 20)
 remote_developer = RemoteDeveloper('Давид','Сеньёр разработчик','Python','Латинская Америка')
 # print(employee.get_info())
-print(manager.get_info())
-print(developer.get_info())
-print(remote_developer.location)
+# print(manager.get_info())
+# print(developer.get_info())
+print(remote_developer.get_info())
 print(RemoteDeveloper.__mro__)
